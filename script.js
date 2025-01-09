634ca15f56be45e42f0267898551c597857bae39
Jansso <46106000+Jansso92@users.noreply.github.com>
2025-01-09 03:57:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
-// Funktion zum Einfügen des Texts in das Eingabefeld
+// Funktion zum Einfügen von Text an der aktuellen Cursorposition im Textarea
 function insertText(text) {
-    var textarea = document.getElementById('itemlore');
-    var start = textarea.selectionStart;
-    var end = textarea.selectionEnd;
-
-    // Füge den Text an der Position des Cursors ein
-    textarea.value = textarea.value.substring(0, start) + text + textarea.value.substring(end);
-
-    // Setze den Cursor nach dem eingefügten Text
-    textarea.selectionStart = textarea.selectionEnd = start + text.length;
+    const textarea = document.getElementById('itemlore');
+    const cursorPos = textarea.selectionStart;
+    const textBefore = textarea.value.substring(0, cursorPos);
+    const textAfter = textarea.value.substring(cursorPos);
+    textarea.value = textBefore + text + textAfter;
+    // Cursor nach dem eingefügten Text positionieren
+    textarea.selectionStart = textarea.selectionEnd = cursorPos + text.length;
     textarea.focus();
-
-    // Vorschau aktualisieren
     updatePreview();
 }
 
 // Funktion zum Aktualisieren der Tooltip-Vorschau
 function updatePreview() {
-    var loreText = document.getElementById('itemlore').value;
-    var preview = document.getElementById('tooltip-lore-preview');
-
-    // Ersetze Minecraft-Farbcodes und Effekte durch HTML-Tags
-    var formattedText = loreText
-        .replace(/&0/g, '<span style="color: #000000">') // Schwarz
-        .replace(/&1/g, '<span style="color: #0000AA">') // Dunkelblau
-        .replace(/&2/g, '<span style="color: #00AA00">') // Dunkelgrün
-        .replace(/&3/g, '<span style="color: #00AAAA">') // Dunkelaqua
-        .replace(/&4/g, '<span style="color: #AA0000">') // Dunkelrot
-        .replace(/&5/g, '<span style="color: #AA00AA">') // Lila
-        .replace(/&6/g, '<span style="color: #FFAA00">') // Gold
-        .replace(/&7/g, '<span style="color: #AAAAAA">') // Grau
-        .replace(/&8/g, '<span style="color: #555555">') // Dunkelgrau
-        .replace(/&9/g, '<span style="color: #5555FF">') // Blau
-        .replace(/&a/g, '<span style="color: #00FF00">') // Grün
-        .replace(/&b/g, '<span style="color: #55FFFF">') // Aqua
-        .replace(/&c/g, '<span style="color: #FF5555">') // Rot
-        .replace(/&d/g, '<span style="color: #FF55FF">') // Rosa
-        .replace(/&e/g, '<span style="color: #FFFF55">') // Gelb
-        .replace(/&f/g, '<span style="color: #FFFFFF">') // Weiß
-        .replace(/&l/g, '<span style="font-weight: bold">') // Fett
-        .replace(/&o/g, '<span style="font-style: italic">') // Kursiv
-        .replace(/&n/g, '<span style="text-decoration: underline">') // Unterstrichen
-        .replace(/&m/g, '<span style="text-decoration: line-through">') // Durchgestrichen
-        .replace(/&r/g, '<span>'); // Reset
-
-    // Setze das HTML der Vorschau
-    preview.innerHTML = formattedText + '</span>';
+    const loreText = document.getElementById('itemlore').value;
+    // Text mit Farbcodes und Effekten für die Vorschau formatieren
+    const formattedText = formatText(loreText);
+    document.getElementById('tooltip-lore-preview').innerHTML = formattedText;
 }
 
-// Funktion zum Kopieren des Texts in die Zwischenablage
+// Funktion zum Formatieren des Textes (unterstützt Minecraft Farbcodes und Effekte)
+function formatText(text) {
+    const colorCodeMap = {
+        '&0': '<span style="color: #000000">', '&1': '<span style="color: #0000AA">',
+        '&2': '<span style="color: #00AA00">', '&3': '<span style="color: #00AAAA">',
+        '&4': '<span style="color: #AA0000">', '&5': '<span style="color: #AA00AA">',
+        '&6': '<span style="color: #FFAA00">', '&7': '<span style="color: #AAAAAA">',
+        '&8': '<span style="color: #555555">', '&9': '<span style="color: #5555FF">',
+        '&a': '<span style="color: #55FF55">', '&b': '<span style="color: #55FFFF">',
+        '&c': '<span style="color: #FF5555">', '&d': '<span style="color: #FF55FF">',
+        '&e': '<span style="color: #FFFF55">', '&f': '<span style="color: #FFFFFF">',
+        '&l': '<span style="font-weight: bold">', '&o': '<span style="font-style: italic">',
+        '&n': '<span style="text-decoration: underline">', '&m': '<span style="text-decoration: line-through">',
+        '&r': '<span style="font-weight: normal; font-style: normal; text-decoration: none">'
+    };
+
+    // Ersetze Farbcodes & Effekte
+    let formattedText = text;
+    Object.keys(colorCodeMap).forEach(code => {
+        const regex = new RegExp(code, 'g');
+        formattedText = formattedText.replace(regex, colorCodeMap[code] + '$&</span>');
+    });
+
+    return formattedText;
+}
+
+// Funktion zum Kopieren des Textes in die Zwischenablage
 function copyToClipboard() {
-    var textarea = document.getElementById('itemlore');
-    textarea.select();
+    const loreText = document.getElementById('itemlore').value;
+    const tempTextarea = document.createElement('textarea');
+    tempTextarea.value = loreText;
+    document.body.appendChild(tempTextarea);
+    tempTextarea.select();
     document.execCommand('copy');
+    document.body.removeChild(tempTextarea);
+    alert("Text in die Zwischenablage kopiert!");
 }
-
-// Initiale Vorschau anzeigen
-updatePreview();
